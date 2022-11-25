2af4e095d25709d32daa42e3e454464a32309eab
AndrewWalsh <softwaredevservices@pm.me>
2022-11-24 22:13:04 -0800

    update docs

--- demo/src/init-msw.ts
+++ demo/src/init-msw.ts
@@ -3,18 +3,17 @@ import { SetupWorkerApi } from "msw";
 import { startAtYourService } from "at-your-service";
 // import { startAtYourService } from "../../src";
 
-// const LOCALHOST_API = "http://localhost:8080";
-// const SW_PATH = `${import.meta.env.BASE_URL}mockServiceWorker.js`;
+const SW_PATH = `${import.meta.env.BASE_URL}mockServiceWorker.js`;
+// window.navigator.serviceWorker.register(SW_PATH);
 
-const initialiseWorker = (worker: SetupWorkerApi) => {
-  // await window.navigator.serviceWorker.register(SW_PATH);
+const initialiseWorker = async (worker: SetupWorkerApi) => {
+  await window.navigator.serviceWorker.register(SW_PATH);
 
   worker.start({
     findWorker(scriptUrl) {
       return scriptUrl.includes("mockServiceWorker.js");
     },
-    quiet: true,
-    waitUntilReady: true,
+    quiet: false,
   });
 
   window.navigator.serviceWorker.ready.then(() => {
@@ -25,10 +24,12 @@ const initialiseWorker = (worker: SetupWorkerApi) => {
 };
 
 let worker: SetupWorkerApi;
-export default (setupWorker: SetupWorkerApi) => {
-  if (worker) {
+let didCall = false
+export default async (setupWorker: SetupWorkerApi) => {
+  if (didCall) {
     return worker
   }
-  worker = initialiseWorker(setupWorker)
+  didCall = true
+  worker = await initialiseWorker(setupWorker)
   return worker
 }
