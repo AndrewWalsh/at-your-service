0c5df9fba68222ea1a5f30e0aed480e4ee56b471
AndrewWalsh <softwaredevservices@pm.me>
2022-11-28 02:11:15 -0800

    update

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App";
 import "./styles.css";
 
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
