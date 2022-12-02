8688379db976d8bda8e844f7f36bede697825d70
AndrewWalsh <softwaredevservices@pm.me>
2022-12-02 01:08:37 -0800

    v0.1.14

--- src/lib/samples-to-json-schema.test.ts
+++ src/lib/samples-to-json-schema.test.ts
@@ -4,7 +4,22 @@ import toJSONSchema from "./samples-to-json-schema";
 import { Sample } from "../data-types";
 
 test("combines string sample into a string JSON schema", async () => {
-  const sample: Sample = new Sample('"test"');
-  const result = toJSONSchema([sample]);
-  expect(result).toEqual({ type: "string" });
+  const objOne = {
+    test: "string",
+  };
+  const objTwo = {
+    test: 1,
+  };
+  const sampleOne: Sample = new Sample(JSON.stringify(objOne));
+  const sampleTwo: Sample = new Sample(JSON.stringify(objTwo));
+  const result = toJSONSchema([sampleOne, sampleTwo]);
+  expect(result).toEqual({
+    properties: {
+      test: {
+        type: ["integer", "string"],
+      },
+    },
+    required: ["test"],
+    type: "object",
+  });
 });
