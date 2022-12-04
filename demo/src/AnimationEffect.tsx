dc1d692feeaa0023099e08054fffaf925e26a1f6
AndrewWalsh <softwaredevservices@pm.me>
2022-12-04 00:43:35 -0800

    style update

--- demo/src/AnimationEffect.tsx
+++ demo/src/AnimationEffect.tsx
@@ -1,5 +1,5 @@
 import { Box, keyframes } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { uniqueId, random } from "lodash";
 
 import { COLOR_PRIMARY, COLOR_SECONDARY } from "./constants";
@@ -30,47 +30,53 @@ type Props = {
 
 function AnimationEffect({ position }: Props) {
   const [items, setItems] = useState<Array<Item>>([]);
+
+  const calculateItems = useCallback(() => {
+    const howMany = Math.floor(Math.random() * QUANTITY_TO_GEN) + 1;
+    const newItems: Array<Item> = [];
+    for (let i = 0; i < howMany; i++) {
+      const height = Math.floor(Math.random() * 40) + 1;
+      // Bias towards top items from distribution
+      const topItems: Array<number> = [];
+      for (let i = 0; i < 2; i++) {
+        topItems.push(Math.floor(Math.random() * 90));
+      }
+      topItems.sort();
+      const heightPx = `${Math.ceil(height)}px`;
+      const offset = position === "left" ? `-` : `+`;
+      newItems.push({
+        id: uniqueId(),
+        height: heightPx,
+        width: heightPx,
+        // Bias towards the top
+        top: `${topItems[-1] + 1}%`,
+        horizontal: `calc(${
+          Math.floor(Math.random() * 100) + 1
+        }% ${offset} ${Math.ceil(height)}px)`,
+      });
+    }
+    setItems(newItems);
+  }, [items]);
+
   useEffect(() => {
     const timeout = setTimeout(() => {
-      const howMany = Math.floor(Math.random() * QUANTITY_TO_GEN) + 1;
-      const newItems: Array<Item> = [];
-      for (let i = 0; i < howMany; i++) {
-        const height = Math.floor(Math.random() * 40) + 1;
-        // Bias towards top items from distribution
-        const topItems: Array<number> = [];
-        for (let i = 0; i < 2; i++) {
-          topItems.push(Math.floor(Math.random() * 90));
-        }
-        topItems.sort();
-        const heightPx = `${Math.ceil(height)}px`;
-        const offset = position === "left" ? `-` : `+`;
-        newItems.push({
-          id: uniqueId(),
-          height: heightPx,
-          width: heightPx,
-          // Bias towards the top
-          top: `${topItems[-1] + 1}%`,
-          horizontal: `calc(${
-            Math.floor(Math.random() * 100) + 1
-          }% ${offset} ${Math.ceil(height)}px)`,
-        });
-      }
-      setItems(newItems);
+      calculateItems();
     }, DURATION * 1000);
     return () => clearTimeout(timeout);
   }, [items]);
+
+  useEffect(() => {
+    calculateItems();
+  }, []);
   return (
     <Box height="100%" width="100%" position="absolute">
       {items.map((item) => (
         <Box
           key={item.id}
           borderColor={
-            [
-              COLOR_PRIMARY,
-              COLOR_PRIMARY,
-              COLOR_PRIMARY,
-              COLOR_SECONDARY,
-            ][random(0, 3)]
+            [COLOR_PRIMARY, COLOR_PRIMARY, COLOR_PRIMARY, COLOR_SECONDARY][
+              random(0, 3)
+            ]
           }
           borderStyle={
             ["dashed", "dotted", "double", "solid", "ridge", "groove"][
