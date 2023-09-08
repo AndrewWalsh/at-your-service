3cd93f4b8230c9ebaee4d126fd471d7962b134ef
AndrewWalsh <softwaredevservices@pm.me>
2023-09-07 23:55:03 -0700

    simpler build step

--- demo/vite.config.build.ts
+++ demo/vite.config.build.ts
@@ -1,47 +1,17 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
 
 import { defineConfig } from "vite";
 
+
+// To install
+// Ensure ./src/init-msw.ts imports from "at-your-service";
+// Run commands from the project root dir:
+// npm run build && npm link at-your-service && cd demo && npm run build:docs
 export default defineConfig({
-  // Uncomment to build docs that will work with GitHub Pages
-  // base: "/at-your-service",
-  // define: {
-  //   global: "window",
-  // },
   plugins: [
     react(),
   ],
-  // resolve: {
-  //   alias: {
-  //     buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
-  //     process: "rollup-plugin-node-polyfills/polyfills/process-es6",
-  //     global: "rollup-plugin-node-polyfills/polyfills/global-es6",
-  //   },
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     define: {
-  //       global: "window",
-  //     },
-  //     plugins: [
-  //       NodeGlobalsPolyfillPlugin({
-  //         process: true,
-  //         buffer: true,
-  //       }),
-  //       NodeModulesPolyfillPlugin(),
-  //     ],
-  //   },
-  // },
-  // build: {
-  //   rollupOptions: {
-  //     // @ts-ignore
-  //     plugins: [nodePolyfills()],
-  //   },
-  // },
 });
